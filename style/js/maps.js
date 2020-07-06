e7fc9a4353fdb1f9227700b570e7e3fbe35d72c2
arthurbonilha <54541910+arthurbonilha@users.noreply.github.com>
2020-07-06 19:24:39 -0300

    correção de bug e troca de icon

--- style/js/maps.js
+++ style/js/maps.js
@@ -1,68 +1,67 @@
-(function (exports) {
-    function initAutocomplete() {
-        var map = new google.maps.Map(document.getElementById("map"), {
-            center: {
-                lat: -23.5734614,
-                lng: -46.9806374
-            },
-            zoom: 13,
-            mapTypeId: "roadmap"
-            
+function initAutocomplete() {
+    var map = new google.maps.Map(document.getElementById("map"), {
+        center: {
+            lat: -23.5734614,
+            lng: -46.9806374
+        },
+        zoom: 14,
+        mapTypeId: "roadmap"
+
+    });
+
+    var input = document.getElementById("buscar");
+    var searchBox = new google.maps.places.SearchBox(input);
+    map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+
+    map.addListener("bounds_changed", function () {
+        searchBox.setBounds(map.getBounds());
+    });
+    var markers = [];
+
+    searchBox.addListener("places_changed", function () {
+        var places = searchBox.getPlaces();
+
+        if (places.length == 0) {
+            return;
+        }
+
+        markers.forEach(function (marker) {
+            marker.setMap(null);
         });
+        markers = [];
 
-        var input = document.getElementById("buscar");
-        var searchBox = new google.maps.places.SearchBox(input);
-        map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
-
-        map.addListener("bounds_changed", function () {
-            searchBox.setBounds(map.getBounds());
-        });
-        var markers = []; 
-
-        searchBox.addListener("places_changed", function () {
-            var places = searchBox.getPlaces();
-
-            if (places.length == 0) {
+        var bounds = new google.maps.LatLngBounds();
+        places.forEach(function (place) {
+            if (!place.geometry) {
+                console.log("Local sem geometria");
                 return;
-            } 
+            }
 
-            markers.forEach(function (marker) {
-                marker.setMap(null);
-            });
-            markers = []; 
+            var icon = {
+                url: place.icon,
+                size: new google.maps.Size(71, 71),
+                origin: new google.maps.Point(0, 0),
+                anchor: new google.maps.Point(17, 34),
+                scaledSize: new google.maps.Size(25, 25)
+            };
 
-            var bounds = new google.maps.LatLngBounds();
-            places.forEach(function (place) {
-                if (!place.geometry) {
-                    console.log("Local sem geometria");
-                    return;
-                }
+            markers.push(
+                new google.maps.Marker({
+                    map: map,
+                    draggable: true,
+                    title: place.name,
+                    position: place.geometry.location,
+                    animation: google.maps.Animation.DROP,
+                })
+            );
 
-                var icon = {
-                    url: place.icon,
-                    size: new google.maps.Size(71, 71),
-                    origin: new google.maps.Point(0, 0),
-                    anchor: new google.maps.Point(17, 34),
-                    scaledSize: new google.maps.Size(25, 25)
-                }; 
-
-                markers.push(
-                    new google.maps.Marker({
-                        map: map,
-                        icon: icon,
-                        title: place.name,
-                        position: place.geometry.location
-                    })
-                );
-
-                if (place.geometry.viewport) {
-                    bounds.union(place.geometry.viewport);
-                } else {
-                    bounds.extend(place.geometry.location);
-                }
-            });
-            map.fitBounds(bounds);
+            if (place.geometry.viewport) {
+                bounds.union(place.geometry.viewport);
+            } else {
+                bounds.extend(place.geometry.location);
+            }
         });
-    }
-    exports.initAutocomplete = initAutocomplete;
-})((this.window = this.window || {}));
+        map.fitBounds(bounds);
+    });
+}
+exports.initAutocomplete = initAutocomplete;
